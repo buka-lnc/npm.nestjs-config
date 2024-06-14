c3d6b98bf4f3fc6d7f1308f99d40dffa3113e293
val.istar.guo <val.istar.guo@gmail.com>
2024-06-14 12:08:49 +0800

    fix: avoid conflicts caused by useless ts definitions

--- src/interfaces/async-options.ts
+++ src/interfaces/async-options.ts
@@ -1,10 +1,4 @@
-import { FactoryProvider } from '@nestjs/common'
-
-
 export interface AsyncOptions {
-  inject?: FactoryProvider['inject']
-
   useFactory<T extends object>(...args: any[]): T
   useFactory<T extends object>(...args: any[]): Promise<T> | T
 }
-
