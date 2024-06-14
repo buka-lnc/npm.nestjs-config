472013ec320ae7166584be58515a9b5f0068a23c
val.istar.guo <val.istar.guo@gmail.com>
2024-06-14 11:41:54 +0800

    fix: compatible with useFacotry without Promise

--- src/interfaces/async-options.ts
+++ src/interfaces/async-options.ts
@@ -3,6 +3,8 @@ import { FactoryProvider } from '@nestjs/common'
 
 export interface AsyncOptions {
   inject?: FactoryProvider['inject']
-  useFactory: <T extends object>(...args: any[]) => Promise<T> | T
+
+  useFactory<T extends object>(...args: any[]): T
+  useFactory<T extends object>(...args: any[]): Promise<T> | T
 }
 
